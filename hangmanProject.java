36eb0b6fb7a40e8bfd9e2a356f4cf26a1f72b3f7
quantumstarr <chriscmark@gmail.com>
2017-04-19 14:47:27 -0400

    Update hangmanProject.java

--- hangmanProject.java
+++ hangmanProject.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
 
-/**
- * Write a description of class hangmanProject here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class hangmanProject
+public class Hangman
 {
+    
+
     public static void main (String args[])
     {
+        Scanner answer = new Scanner (System.in);
+        boolean gameOver = false;
+        boolean win = false;
+        
+        
+        while (gameOver == false)
+        {
+            answer.next();
+            if(answer.length != 1)
+            {
+                
+            }
+            
+        }
+        
+        if (win == true)
+        {
+            
+        }
+    }
+
+    
+    public boolean checkAnswer(String answer)
+    {
+       
+    }
+    
+    public void printHangman(int timesWrong)
+    {
+        System.out.println("Print some hangman Ascii art here");
     }
 }
